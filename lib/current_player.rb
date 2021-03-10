3acb272f6a9125805152ec97950aafbc2153f392
Erika Curry-Elrod <ecurryelrod@gmail.com>
2021-03-10 20:55:01 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,8 +1,7 @@
 def turn_count(board)
   counter = 0
   board.each do |space|
-    if space == "X" || space == "O"
-      counter += 1
-    end
+    space == "X" || space == "O"
+    counter += 1
   end
 end
